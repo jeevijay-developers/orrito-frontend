a5e5c1d820cf5a0f4c118506d969eb95a0787e46
Abhishek Yadav <monuy8830@gmail.com>
2025-06-04 15:19:22 +0530

    Refactor product query page to use Suspense for loading and encapsulate query form logic in a separate component

--- app/(pages)/productquery/page.jsx
+++ app/(pages)/productquery/page.jsx
@@ -3,7 +3,7 @@
 import FooterSeven from "@/components/layout/footers/FooterSeven";
 import Header9 from "@/components/layout/header/Header9";
 import ProductQueryForm from "@/components/pages/productQuery/ProductQueryForm";
-import React from "react";
+import React, { Suspense } from "react";
 import Hero from "@/components/pages/productQuery/Hero";
 
 import { useSearchParams } from "next/navigation";
@@ -12,11 +12,18 @@ import { useSearchParams } from "next/navigation";
 //   title: "Query || Oritto LED Lights",
 //   description: "Oritto LED Lights",
 // };
-
-export default function page() {
+function QueryFormWrapper() {
   const searchParams = useSearchParams();
   const productId = searchParams.get("productId");
   const productName = searchParams.get("productName");
+
+  return <ProductQueryForm productId={productId} productName={productName} />;
+}
+
+export default function page() {
+  // const searchParams = useSearchParams();
+  // const productId = searchParams.get("productId");
+  // const productName = searchParams.get("productName");
   // console.log("Product ID:", productId);
   // console.log("Product Name:", productName);
   
@@ -25,7 +32,10 @@ export default function page() {
       <main>
         <Header9 />
         <Hero />
-        <ProductQueryForm productId={productId} productName={productName} />
+        {/* <ProductQueryForm productId={productId} productName={productName} /> */}
+        <Suspense fallback={<div>Loading...</div>}>
+          <QueryFormWrapper />
+        </Suspense>
         <FooterSeven />
       </main>
     </>
