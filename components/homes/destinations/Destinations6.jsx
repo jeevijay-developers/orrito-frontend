c9fda0a9e262fc28d459e1dda4bf23da6fb1ac68
iCodeLakshay <lakshaysaxena2217@gmail.com>
2025-06-04 12:31:35 +0530

    dummy push

--- components/homes/destinations/Destinations6.jsx
+++ components/homes/destinations/Destinations6.jsx
@@ -10,7 +10,7 @@ export default function Destinations6() {
         <div className="row y-gap-10 justify-between items-end">
           <div className="col-auto">
             <h2 data-aos="fade-up" data-aos-delay="" className="text-30">
-              Trending Destinations
+              Trending xyz
             </h2>
           </div>
 
